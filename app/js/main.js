1f47088ca759b02207f736284727ef259b90ab69
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2021-08-01 21:23:52 +0600

    Add Search

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,16 @@ $(function () {
     nextArrow:
       '<button class="banner-slider-btn banner-slider-btn-next"><img src="images/arrow-right.svg" alt=""></button>',
   });
+
+  const tabs = $(".search__tabs-item");
+
+  tabs.on("click", function (e) {
+    e.preventDefault();
+
+    tabs.removeClass("search__tabs-item--active");
+    $(".search__content-item").removeClass("search__content-item--active");
+
+    $(this).addClass("search__tabs-item--active");
+    $($(this).attr("href")).addClass("search__content-item--active");
+  });
 });
