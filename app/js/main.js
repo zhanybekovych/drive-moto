4418ff38891ef13d5b65b40a03cb4ac41d1c20b6
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2021-08-01 18:48:37 +0600

    Add Banner

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-$(document).ready(function () {});
+$(function () {
+  $(".banner__slider").slick({
+    dots: true,
+    prevArrow:
+      '<button class="banner-slider-btn banner-slider-btn-prev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow:
+      '<button class="banner-slider-btn banner-slider-btn-next"><img src="images/arrow-right.svg" alt=""></button>',
+  });
+});
