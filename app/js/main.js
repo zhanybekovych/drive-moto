10e7635b7d3ae5a032b87f497362834c80b26d77
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2021-08-11 18:56:30 +0600

    Add catalog

--- app/js/main.js
+++ app/js/main.js
@@ -37,4 +37,35 @@ $(function () {
       target.find(".products__slider").slick("refresh");
     }
   });
+
+  $(".filter-style").styler();
+
+  $(".catalog__filter-drop, .catalog__filter-param").on("click", function () {
+    $(this).toggleClass("catalog__filter-drop--active");
+    $(this).next().slideToggle("fast");
+  });
+
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    grid: false,
+    min: 100000,
+    max: 500000,
+    from: 150000,
+    to: 300000,
+    hide_from_to: true,
+  });
+
+  $(".catalog__sort-view--grid").on("click", function () {
+    $(this).addClass("catalog__sort-view--active");
+    $(".catalog__sort-view--line").removeClass("catalog__sort-view--active");
+    $(".products__item-wrapper").removeClass(
+      "products__item-wrapper-list-view"
+    );
+  });
+
+  $(".catalog__sort-view--line").on("click", function () {
+    $(this).addClass("catalog__sort-view--active");
+    $(".catalog__sort-view--grid").removeClass("catalog__sort-view--active");
+    $(".products__item-wrapper").addClass("products__item-wrapper-list-view");
+  });
 });
